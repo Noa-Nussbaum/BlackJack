8329babc40d9772bfc3e1b7094fdbda9e250fbb6
unknown <noa.nussb@gmail.com>
2025-05-06 14:18:09 +0300

    Completed

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function getRandom(){
-    return Math.floor(Math.random() * 11) + 2;
-}
-let firstCard = getRandom();
-let secondCard = getRandom();
-let hasJackBlack = false
-let isAlive = true
 
-let sum = firstCard + secondCard;
-let cards = [firstCard, secondCard]
+let player = {
+    name: "Noa",
+    chips: 0
+}
+
+
+function getRandomCard(){
+    let card =  Math.floor(Math.random() * 13) + 1
+    if(card > 10){
+        return 10
+    }
+    if(card === 1){
+        return 11
+    }
+    return card
+}
+
+// Global Variables
+let hasJackBlack = false
+let isAlive = false
+let sum = 0
+let cards = []
+let message = ""
 
 // Render
 let messageEl = document.getElementById("message-el")
@@ -15,31 +29,46 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+    isAlive = true
+    hasJackBlack = false
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+
+    sum = firstCard + secondCard;
+    cards = [firstCard, secondCard]
     renderGame()
 }
 
 function renderGame(){
 
-sumEl.textContent = "Sum: " + sum
-cardsEl.textContent = "Cards: "+ cards 
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: "+ cards 
 
-if (sum <= 20) {
-    messageEl.textContent = "Do you want to draw a new card?";
-}else if(sum === 21) {
-    hasJackBlack = true
-    messageEl.textContent = "Blackjack!";
-} else {
-    isAlive = false
-    messageEl.textContent = "You are out of the game!";
-}
-
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    }else if(sum === 21) {
+        hasJackBlack = true
+        message = "Blackjack!";
+        player.chips += 10
+        playerEl.textContent = player.name + ": $" + player.chips
+    } else {
+        isAlive = false
+        message = "You are out of the game!";
+    }
+        messageEl.textContent = message
 }
 
 function newCard(){
-    let thirdCard = getRandom()
-    cards.push(thirdCard)
-    sum += thirdCard
-    
-    renderGame()
+    if(isAlive === true && hasJackBlack === false){
+        console.log("Drawing a new card from the deck!")
+        let card = getRandomCard()
+        cards.push(card)
+        sum += card
+        renderGame()
+    }
 }
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 
